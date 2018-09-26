1642a9525908bbe20d68e7d9da960519914570b3
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-26 14:20:07 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    if @roster.include?(grade) == true
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+
+  def grade(student_grade)
+    @roster[student_grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+    @roster
+    end
+
+end
