e8bbf1160fde240ef8eb62cfd3879f76300086a9
Saint-Jean Germila <germila@noukod.com>
2018-10-03 19:18:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,26 @@
 class School
-def roster(roster)
-  @roster = roster
-end
-def roster
-  @roster
-end
+  attr_accessor :name, :roster
 
-end
-school = School.new("Bayside High School")
-school.roster
+  def initialize(name)
+   @name = name
+   @roster = {}
+  end
+
+  def add_student(student_name, grade)
+      roster[grade] ||= []
+      roster[grade] << student_name
+    end
+
+    def grade(student_grade)
+      roster[student_grade]
+    end
+
+    # this method should arrange the students in each grade by alphabetical order
+    def sort
+      sorted = {}
+      roster.each do |grade, students|
+        sorted[grade] = students.sort
+      end
+      sorted
+    end
+  end
