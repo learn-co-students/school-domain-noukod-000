1938f6cca2fca9fcee368bad1bf9d840a5c8cca1
Ricardo Paul <kingupper0@gmail.com>
2019-04-22 17:14:40 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+#name and roster are methods that are initialized be the initialize method
+# name has an instance variable, and roster an instance hash
+   attr_accessor :name, :roster
+
+   def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+     # we will empty the grade unless there's already a grade in the place
+
+   def add_student(name, grade)
+    roster[grade] = [] unless roster[grade]
+    roster[grade] << name
+  end
+
+   def grade(num)
+    roster[num]
+  end
+
+   def sort
+    sorted = {}
+    roster.each do |key, values|
+      sorted[key] = values.sort
+    end
+    sorted
+  end
+
+ end 
