15079ac145109b3c357c865852d8bcca45720bcb
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-23 16:13:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  attr_reader :roster, :name
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(val,cle)
+    @roster[cle] = [] unless @roster.key?(cle)
+    @roster[cle] << val
+  end 
+  
+  def grade(cle)
+    @roster[cle]
+  end 
+  
+  def sort_student(a)
+    grade(a).sort
+  end 
+  
+  def sort 
+   Hash[@roster.collect{|a| [a.first,sort_student(a.first)]}]
+  end
+end 
